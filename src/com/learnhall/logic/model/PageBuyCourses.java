509e29cd7cc429bef3cc2aa6c0a35d31e144810c
Drifter <379657505@qq.com>
2016-08-18 17:46:40 +0800

    修复下以前购买的套餐信息,这个地方下次不需要修复,的设置isRepair=false

--- src/com/learnhall/logic/model/PageBuyCourses.java
+++ src/com/learnhall/logic/model/PageBuyCourses.java
@@ -3,8 +3,10 @@ package com.learnhall.logic.model;
 import java.util.List;
 import java.util.Map;
 
+import com.bowlong.util.ListEx;
 import com.bowlong.util.page.APage;
 import com.learnhall.db.bean.Boughtkinds;
+import com.learnhall.db.bean.Kind;
 import com.learnhall.db.entity.BoughtkindsEntity;
 
 public class PageBuyCourses extends APage<Boughtkinds> {
@@ -27,9 +29,49 @@ public class PageBuyCourses extends APage<Boughtkinds> {
 		int limit = pageSize;
 		begin = begin < 0 ? 0 : begin;
 		try {
-			return BoughtkindsEntity.getListBy(params, begin, limit);
+			List<Boughtkinds> list = BoughtkindsEntity.getListBy(params, begin,
+					limit);
+			repair(list);
+			return list;
 		} catch (Exception e) {
 			return null;
+		}
+	}
+
+	// 修复下以前购买的套餐信息
+	boolean isRepair = true;
+
+	public void repair(List<Boughtkinds> list) {
+		if (!isRepair)
+			return;
+
+		if (!ListEx.isEmpty(list)) {
+			int lens = list.size();
+			Boughtkinds tmp;
+			Kind kind;
+			String name;
+			double price;
+			boolean isUp = false;
+			for (int i = 0; i < lens; i++) {
+				tmp = list.get(i);
+				kind = tmp.getKindFkKindid();
+				isUp = false;
+				if (kind != null) {
+					name = kind.getNmProduct();
+					price = kind.getPrice();
+					if (price != tmp.getPrice()) {
+						tmp.setPrice(price);
+						isUp = true;
+					}
+					if (!name.equals(tmp.getName())) {
+						tmp.setName(name);
+						isUp = true;
+					}
+				}
+				if (isUp) {
+					tmp.update();
+				}
+			}
 		}
 	}
 }
